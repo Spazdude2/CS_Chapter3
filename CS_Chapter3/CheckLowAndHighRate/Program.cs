45bd65fc4c91fa0fdda8de910b32a7354a2af505
Spazdude2 <spaziani96@gmail.com>
2017-02-15 13:49:09 -0500

    Check low and high rate

--- CS_Chapter3/CheckLowAndHighRate/Program.cs
+++ CS_Chapter3/CheckLowAndHighRate/Program.cs
@@ -9,6 +9,22 @@ namespace CheckLowAndHighRate
     {
         static void Main(string[] args)
         {
+            int pay = Convert.ToInt32(Console.ReadLine());
+
+            if (pay < 5.56)
+            {
+                Console.WriteLine("ERROR!");
+            }
+
+            else if (pay > 49.99)
+            {
+                Console.WriteLine("ERROR!");
+            }
+
+            else
+            {
+                Console.WriteLine("Well Done.");
+            }
         }
     }
 }
